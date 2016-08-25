566a1ee0532ac5040a5959ca871edd96b9e4285c
unknown <iramsey@WS-I3-32.WS-ACADBUS.EDU>
2016-08-25 08:26:15 -0700

    Fist Commit

---
+++ Project 2 - First Class/FCRunnner.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class FCRunnner here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class FCRunnner extends FirstClass
+{
+    // instance variables - replace the example below with your own
+
+    /**
+     * Constructor for objects of class FCRunnner
+     */
+    public static void main(String[] args)
+    {
+       FirstClass fc = new FirstClass();
+       System.out.println(fc.add(1,2));
+       System.out.println(fc.sub(2,1));
+    
+    }
+}
---
+++ Project 2 - First Class/FirstClass.java
@@ -0,0 +1,36 @@
+
+/**
+ * Write a description of class FirstClass here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class FirstClass
+{
+    // instance variables - replace the example below with your own
+    private int x;
+    private int y;
+
+    /**
+     * Constructor for objects of class FirstClass
+     */
+    public FirstClass()
+    {
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int add(int x, int y)
+    {
+        // put your code here
+        return x + y;
+    }
+    public int sub(int x, int y){
+        return x - y;
+     
+    }
+}
